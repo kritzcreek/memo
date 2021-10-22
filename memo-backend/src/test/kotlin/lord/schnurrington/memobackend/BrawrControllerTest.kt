5675c6795aceb4fc136738cb5f1ed2544b1b69ac
Mara Hegemann <mara.braun@web.de>
2021-10-22 20:27:44 +0200

    upgrade versions

--- memo-backend/src/test/kotlin/lord/schnurrington/memobackend/BrawrControllerTest.kt
+++ memo-backend/src/test/kotlin/lord/schnurrington/memobackend/BrawrControllerTest.kt
@@ -7,10 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.client.match.MockRestRequestMatchers.content
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.web.servlet.function.RequestPredicates.contentType
 
 @SpringBootTest
 @AutoConfigureMockMvc
