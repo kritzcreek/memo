24c59bb88e92279ade72993dafe886c101e4ffc9
Mara Hegemann <mara.braun@web.de>
2021-10-31 15:06:28 +0100

    add db connection to application (#9)

--- src/main/kotlin/lord/schnurrington/memobackend/MemoBackendApplication.kt
+++ src/main/kotlin/lord/schnurrington/memobackend/MemoBackendApplication.kt
@@ -2,10 +2,16 @@ package lord.schnurrington.memobackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.jetbrains.exposed.sql.Database
 
 @SpringBootApplication
-open class MemoBackendApplication
+class MemoBackendApplication
 
 fun main(args: Array<String>) {
+	val dbUrl = "jdbc:postgresql://localhost:5432/memodb"
+	val dbUser = "nemo"
+	val dbPass = "nemo"
+
+	Database.connect(dbUrl, driver = "org.postgresql.Driver", user = dbUser, password = dbPass)
 	runApplication<MemoBackendApplication>(*args)
 }
