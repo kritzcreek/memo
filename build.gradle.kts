2ea52dcee633bbce086846d1d5d61bd1a0c00f2f
Mara Hegemann <mara.braun@web.de>
2022-10-18 22:26:32 +0200

    use newer gh action for java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.7.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.spring") version "1.5.31"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.spring") version "1.7.20"
 }
 
 group = "lord.schnurrington"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
