32e698defe9c18d00c93052efb3718758dd89f84
Mara Hegemann <mara.braun@web.de>
2022-10-18 21:10:26 +0200

    upgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.5"
+    id("org.springframework.boot") version "2.7.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
@@ -9,22 +9,22 @@ plugins {
 
 group = "lord.schnurrington"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:2.5.5")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.5.5")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("org.springframework.boot:spring-boot-starter:2.7.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("org.postgresql:postgresql:42.2.24.jre7")
-    implementation("org.jetbrains.exposed:exposed-core:0.35.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.35.1")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.5")
+    runtimeOnly("org.postgresql:postgresql:42.5.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.39.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.4")
 }
 
 tasks.withType<Test> {
@@ -34,6 +34,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
